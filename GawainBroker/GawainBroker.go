babff5a588bfd017ac4c8ca77d34374aec5b6e91
miki <miki@osmio.tiscali.sys>
2014-12-22 17:26:26 +0100

    Reactor test

--- GawainBroker/GawainBroker.go
+++ GawainBroker/GawainBroker.go
@@ -8,25 +8,47 @@ import (
 
 const (
 	reqPort = "5555"
+	monPort = "5556"
 )
 
+func Monitor(state zmq.State) {
+	fmt.Println("State: ", state)
+}
+
 func main() {
-	sock, err := zmq.NewSocket(zmq.REP)
+	req, err := zmq.NewSocket(zmq.REP)
 	if err != nil {
 		panic(err)
 	}
-	defer sock.Close()
+	defer req.Close()
 
 	url := fmt.Sprintf("tcp://*:%s", reqPort)
-	if err := sock.Bind(url); err != nil {
+	if err := req.Bind(url); err != nil {
 		panic(err)
 	}
 
-	for {
-		s, _ := sock.Recv(0)
-		if s != "" {
-			fmt.Println(s)
-			sock.Send("PONG", 0)
-		}
+	mon, err := zmq.NewSocket(zmq.REP)
+	if err != nil {
+		panic(err)
 	}
+	defer mon.Close()
+
+	url := fmt.Sprintf("tcp://*:%s", monPort)
+	if err := mon.Bind(url); err != nil {
+		panic(err)
+	}
+
+	monitor = zmq.Reactor()
+	monitor.AddSocket(mon, zmq.POLLIN, Monitor)
+	monitor.Run(time.Second)
+
+	/*
+		for {
+			s, _ := mon.Recv(0)
+			if s != "" {
+				fmt.Println(s)
+				sock.Send("PONG", 0)
+			}
+		}
+	*/
 }
